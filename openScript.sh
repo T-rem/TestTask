a011f78957e915286f04eb1d19a8e4ca842bc406
Alexander Remnev <alexremnev42@gmail.com>
2019-10-17 19:54:15 +0700

    add a working sh script

--- openScript.sh
+++ openScript.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-echo "Input: the quick brown fox jumps over the lazy dog\n"
-echo "Canonical output:
-brown 1
-dog 1
-fox 1
-jumps 1
-lazy 1
-over 1
-quick 1
-the 2\n"
+string=$(head -n 1 ./Test.txt)
+echo " Input: $string \n"
+echo "Expected output:"
+string1=$(sed -n '3,$p' ./Test.txt) 
+echo "$string1"
 cd ./out/artifacts/TestTaskJar
 echo "Application output:"
-java -jar TestTask.jar the quick brown fox jumps over the lazy dog
+java -jar TestTask.jar $string
 
