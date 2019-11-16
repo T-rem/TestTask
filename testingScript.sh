92efe47f937190e90041fff36bfd811411baf73e
Alexander Remnev <alexremnev42@gmail.com>
2019-11-16 12:00:26 +0700

    Изменение скрипта

--- testingScript.sh
+++ testingScript.sh
@@ -1,69 +1,31 @@
 #!/bin/sh
-echo starting comil
+echo starting compil
 kotlinc -cp kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar src/com/ -include-runtime -d application.jar
 echo finish compil
+fail=0
+fn()
+{
+  echo Input: "$1"
+  echo Expected output: "$2"
+  ./run.sh $1
+  res=$?
+  echo Actual output: $res
+  if [ $res -eq $2 ]; then
+    echo Success
+  else
+    echo Failed
+    let fail=fail+1
+  fi
+}
+fn "-login admin -pass admin -res AB.C.D -role EXECUTE" "0"
+fn "-login user1 -pass user1 -res AB.C -role READ" "0"
+fn "-login admin -pass admin -role EXECUTE -res AB.C.D" "0"
+fn "-pass admin -login admin -role EXECUTE -res AB.C.D" "0"
+fn "-login user1 -pass user1 -res AB.C" "0"
+fn "-login admin -pass admin -res АВ -role FULLACCESS" "5"
+fn "-login user1 -pass user1 -res АВ -role READ" "6"
+fn "bla-bla-bla" "0"
 
-echo " Input: -login admin -pass admin -res AB.C.D -role EXECUTE"
-echo " Expected output:
-0"
-echo "Actual output:"
-./run.sh -login admin -pass admin -res AB.C.D -role EXECUTE
-echo "$?"
-
-echo " Input: -login user1 -pass user1 -res AB.C -role READ"
-echo " Expected output:
-0"
-echo "Actual output:"
-./run.sh -login user1 -pass user1 -res AB.C -role READ
-echo "$?"
-
-echo " Input: -login admin -pass admin -role EXECUTE -res AB.C.D"
-echo " Expected output:
-0"
-echo "Actual output:"
-./run.sh -login admin -pass admin -role EXECUTE -res AB.C.D
-echo "$?"
-
-echo " Input: -pass admin -login admin -role EXECUTE -res AB.C.D"
-echo " Expected output:
-0"
-echo "Actual output:"
-./run.sh -pass admin -login admin -role EXECUTE -res AB.C.D
-echo "$?"
-
-echo " Input: -login user1 -pass user1 -res AB.C"
-echo " Expected output:
-0"
-echo "Actual output:"
-./run.sh -login user1 -pass user1 -res AB.C
-
-echo "$?"
-
-echo " Input: -login user1 –pass user -res АВ.C"
-echo " Expected output:
-0"
-echo "Actual output:"
-./run.sh -login user1 -pass user1 -res АВ.C
-echo "$?"
-
-echo " Input: -login admin –pass admin -res АВ –role FULLACCESS "
-echo " Expected output:
-5"
-echo "Actual output:"
-./run.sh -login admin -pass admin -res АВ -role FULLACCESS
-echo "$?"
-
-echo " Input: -login user1 –pass user1 -res АВ –role READ "
-echo " Expected output:
-6"
-echo "Actual output:"
-./run.sh -login user1 -pass user1 -res АВ -role READ
-echo "$?"
-
-echo " Input: xxx"
-echo " Expected output:
-1"
-echo "Actual output:"
-./run.sh xxx
-echo "$?"
-
+if [ $fail -gt 0 ]; then
+  exit 9
+fi
